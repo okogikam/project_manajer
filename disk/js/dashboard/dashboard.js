c5258d20c4b5c9e1b9520363e4bc299437db996d
Okogi_main <okogikam@gmail.com>
2026-06-14 22:10:11 +0800

    menambah fungsi js page

--- disk/js/dashboard/dashboard.js
+++ disk/js/dashboard/dashboard.js
@@ -1,9 +1,34 @@
 class dashboard extends mainScript {
-    async init(){
-        console.log('dashboard');
-        await super.getMetod({db:'dashboard',attr:''}).then(res => {
-            console.log(res);
+    constructor() {
+        super();
+    }
+    async LoadData(){
+        this.project = await super.getMetod({db:'db_project',attr:''});
+        this.task = await super.getMetod({db:'db_task',attr:''});
+    }
+    displayProject(){
+        let card = this.el.querySelector("#card-project");
+        let projectAktif = 0;
+        let projectBaru = 0;
+        this.project.forEach(p => {
+            if(p.is_active == "1"){
+                projectAktif++;
+                let startDate = new Date(p.start_date);
+                if(startDate >= new Date().setDate(new Date().getDate() - 30)){
+                    projectBaru++;
+                }
+            }
         });
+        card.innerHTML = `
+        <div class="card-title">Jumlah Proyek Aktif</div>
+        <div class="card-value">${projectAktif}</div>
+        <div class="card-sub">${projectBaru} proyek baru bulan ini</div>
+        `
+    }
+    async init(){
+        await this.LoadData();
+        this.displayProject();
+        console.log(this.task[0]);
     }
 }
 
--- disk/js/main_script.js
+++ disk/js/main_script.js
@@ -1,4 +1,7 @@
 class mainScript {
+    constructor() {
+        this.el = document.querySelector("body");
+    }
     async getMetod(conf){
         let response = await fetch(`https://script.google.com/macros/s/AKfycbx9xge5bw6ahjMWq-ZzNwarETFlKzEIsH2Xi0uye8ocC1mgno2AqvZY99tH14rcKkpH/exec?db=${conf.db}${conf.attr}`)
         .then(res => res.json());
